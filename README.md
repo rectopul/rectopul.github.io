2af8610440512c178872dc24ea52e40917ae65ee
Neide Dev <neidepereiraantunes@outlook.com>
2022-08-11 15:57:40 -0300

    up page

---
+++ README.md
@@ -0,0 +1,1 @@
+"# rectopul.github.io" 
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 16,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
